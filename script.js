41c31b7fe5b7037c2e2315529434ab67a83eb575
Laura Eguda <lauraeguda@gmailcom>
2021-03-26 10:47:14 -0700

    Genesis

---
+++ script.js
@@ -0,0 +1,17 @@
+var s = document.getElementById("signup")
+var l = document.getElementById("login")
+var b = document.getElementById("button")
+
+
+function login(){
+    s.style.left = "-400px"
+    l.style.left = "50px"
+    b.style.left = "120px"
+}
+function signup(){
+    s.style.left = "50px"
+    l.style.left = "400px"
+    b.style.left = "0px"
+}
+
+ 
